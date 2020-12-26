d956689aa1ba33a0aa4389a57ffd7caa564b343a
Basil Kraft <bkraft@bgc-jena.mpg.de>
2020-12-26 14:58:22 +0100

    moves file

--- tests/dummy_data.py
+++ tests/dummy_data.py
@@ -1,3 +1,4 @@
+"""Dummy data, randomly generated."""
 
 from torch.utils import data
 from pytorch_lightning import LightningDataModule
@@ -107,3 +108,18 @@ class DummyData(LightningDataModule):
 
     def test_dataloader(self):
         return data.DataLoader(DummyDataSimple(), batch_size=self.batch_size)
+
+
+class DummyDataSequence(LightningDataModule):
+    def __init__(self, batch_size):
+        super().__init__()
+        self.batch_size = batch_size
+
+    def train_dataloader(self):
+        return data.DataLoader(DummyDataSequential(), batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return data.DataLoader(DummyDataSequential(), batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return data.DataLoader(DummyDataSequential(), batch_size=self.batch_size)
