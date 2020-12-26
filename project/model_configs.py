186d12314eb7743c90efbc2bcf03a8bbfee46c21
Basil Kraft <bkraft@bgc-jena.mpg.de>
2020-12-26 16:38:07 +0100

    adds global configuration

--- project/model_configs.py
+++ project/model_configs.py
@@ -10,6 +10,37 @@ from utils.pytorch_lighnting import LightningNet
 from utils.config import BaseConfig
 
 
+class HConfig(BaseConfig):
+    """Global model configuration (does not change / is not tunable).
+    """
+    def __init__(self, study_name: str = 'study', batch_size: int = 10, *args, **kwargs):
+        """Model config.
+
+        The batch size can be set manually as different models may require different sized batches.
+
+        Args:
+            study_name (str, optional): the study name. Defaults to `study`.
+            batch_size (int, optional): the batch size. Defaults to 10.
+        """
+        super(HConfig, self).__init__(*args, **kwargs)
+
+        self.STUDY_NAME = study_name
+
+        self.NUM_INPUTS = 1
+        self.NUM_OUTPUTS = 1
+
+        self.BATCH_SIZE = batch_size
+        self.MIN_EPOCHS = 1
+        self.MAX_EPOCHS = 5
+
+        self.SEED = 23427
+
+        # Not saved (bc. lowercase).
+        self.log_freq = 10
+        self.num_workers = 0
+        self.val_loss_name = 'val_loss'
+
+
 def feedforward(config: BaseConfig, trial: optuna.trial.Trial) -> pl.LightningModule:
     """Returns a tunable PyTorch lightning feedforward module.
 
