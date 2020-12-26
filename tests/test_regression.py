a5d52d870e53ad0c568d5d00dbb6b1de15bbedae
Basil Kraft <bkraft@bgc-jena.mpg.de>
2020-12-26 16:40:46 +0100

    removes redundant base configuration

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,44 +1,12 @@
 
 from pytorch_lightning import seed_everything
 
-from project.model_configs import feedforward
+from project.model_configs import feedforward, BaseConfig
 from utils.optuna import Objective, get_study
-from utils.config import BaseConfig
-
-
-class HConfig(BaseConfig):
-    """Global model configuration (does not change / is not tunable).
-    """
-    def __init__(self, study_name: str = 'study', batch_size: int = 10, *args, **kwargs):
-        """Model config.
-
-        The batch size can be set manually as different models may require different sized batches.
-
-        Args:
-            study_name (str, optional): the study name. Defaults to `study`.
-            batch_size (int, optional): the batch size. Defaults to 10.
-        """
-        super(HConfig, self).__init__(*args, **kwargs)
-
-        self.STUDY_NAME = study_name
-
-        self.NUM_INPUTS = 1
-        self.NUM_OUTPUTS = 1
-
-        self.BATCH_SIZE = batch_size
-        self.MIN_EPOCHS = 1
-        self.MAX_EPOCHS = 5
-
-        self.SEED = 23427
-
-        # Not saved (bc. lowercase).
-        self.log_freq = 10
-        self.num_workers = 0
-        self.val_loss_name = 'val_loss'
 
 
 def test_regression(fast_dev_run, resume=False, resume_version='latest'):
-    config = HConfig(resume_policy=resume)
+    config = BaseConfig(resume_policy=resume)
 
     seed_everything(config.SEED)
 
