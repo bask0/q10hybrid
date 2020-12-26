6c9ad1373a96f88221910a1acc0ce4e0c081b350
Basil Kraft <bkraft@bgc-jena.mpg.de>
2020-12-26 16:19:23 +0100

    adds tcn model

--- project/model_configs.py
+++ project/model_configs.py
@@ -3,13 +3,15 @@
 import optuna
 import pytorch_lightning as pl
 
-from project.layer_feedforward import FeedForward
-from project.pl_utils import LightningNet
-from project.utils import BaseConfig
+from models.feedforward import FeedForward
+from models.tcn import TemporalConvNet
+
+from utils.pytorch_lighnting import LightningNet
+from utils.config import BaseConfig
 
 
 def feedforward(config: BaseConfig, trial: optuna.trial.Trial) -> pl.LightningModule:
-    """Returns a tunable PyTorch lightning module.
+    """Returns a tunable PyTorch lightning feedforward module.
 
     Args:
         config (BaseConfig): the hard-coded configuration.
@@ -24,8 +26,37 @@ def feedforward(config: BaseConfig, trial: optuna.trial.Trial) -> pl.LightningMo
         num_outputs=config.NUM_OUTPUTS,
         num_hidden=trial.suggest_int('num_hidden', 1, 4),
         num_layers=trial.suggest_int('num_layers', 1, 2),
-        dropout=trial.suggest_float('dropout', 0.1, 0.3),
+        dropout=trial.suggest_float('dropout', 0.0, 0.5),
         activation=trial.suggest_categorical('activation', ['relu', 'none'])
+    )
+
+    pl_model = LightningNet(
+        model=model,
+        lr=trial.suggest_loguniform('lr', 1e-3, 1e-0),
+        batch_size=config.BATCH_SIZE,
+        max_epochs=config.MAX_EPOCHS
+    )
+
+    return pl_model
+
+
+def tcn(config: BaseConfig, trial: optuna.trial.Trial) -> pl.LightningModule:
+    """Returns a tunable PyTorch lightning tcn module.
+
+    Args:
+        config (BaseConfig): the hard-coded configuration.
+        trial (optuna.Trial): optuna trial.
+
+    Returns:
+        pl.LightningModule: a lightning module.
+    """
+
+    model = TemporalConvNet(
+        num_inputs=config.NUM_INPUTS,
+        num_hidden=trial.suggest_int('num_hidden', 1, 4),
+        kernel_size=trial.suggest_int('kernel_size', 1, 4),
+        num_layers=trial.suggest_int('kernel_size', 1, 2),
+        dropout=trial.suggest_float('dropout', 0.1, 0.3)
     )
 
     pl_model = LightningNet(
