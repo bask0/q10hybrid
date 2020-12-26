7cca240d6b023c863b7b65b615fae6e5879a5acc
Basil Kraft <bkraft@bgc-jena.mpg.de>
2020-12-26 17:34:40 +0100

    initial commit

---
+++ tests/test_tcn.py
@@ -0,0 +1,35 @@
+
+from pytorch_lightning import seed_everything
+
+from project.model_configs import tcn, BaseConfig
+from utils.optuna import Objective, get_study
+
+
+def test_tcn(fast_dev_run, resume=False, resume_version='latest'):
+    config = BaseConfig(resume_policy=resume)
+
+    seed_everything(config.SEED)
+
+    study = get_study(config)
+
+    study.optimize(
+        Objective(config, tcn, wandb_offline=True),
+        n_trials=12 if fast_dev_run else 500)
+
+    print('Best trial:')
+    trial = study.best_trial
+
+    print('  Value: {}'.format(trial.value))
+
+    print('  Params: ')
+    for key, value in trial.params.items():
+        print('    {}: {}'.format(key, value))
+
+    # config.self_destruct_study()
+
+    if trial.value > 0.5:
+        raise AssertionError(f'model run performance above 0.7: {study.best_value}.')
+
+
+if __name__ == "__main__":
+    test_tcn(True, resume=True)
