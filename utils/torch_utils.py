57a0f002a9ccc2d71ddabd535ef90d758c38b536
Basil Kraft <bkraft@bgc-jena.mpg.de>
2020-12-26 14:43:13 +0100

    adds reshaping utils

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,6 +1,7 @@
 
 from torch import nn
 from typing import Union
+from torch import Tensor
 
 
 def get_activation(activation: Union[str, None]) -> nn.Module:
@@ -42,3 +43,66 @@ def get_activation(activation: Union[str, None]) -> nn.Module:
         raise ValueError(
             f'activation `{activation}` not found, chose one of: {choices}.'
         )
+
+
+def is_flat(x: Tensor):
+    """Checks if tensor is in the flat format (batch, sequence x num_features)
+
+    Args:
+        x (Tensor): the tensor.
+    """
+    return x.ndim == 3
+
+
+def is_sequence(x: Tensor):
+    """Checks if tensor is in the sequence format (batch, sequence, num_features)
+
+    Args:
+        x (Tensor): the tensor.
+    """
+    return x.ndim == 2
+
+
+def seq2flat(x: Tensor) -> Tensor:
+    """Reshapes tensor from sequence format to flat format.
+
+    Sequence format: (batch, sequence, features)
+    Flat format: (batch, sequence x features)
+
+    Args:
+        x (Tensor): a tensor in the sequence format (batch, sequence, features).
+
+    Returns:
+        Tensor: the transformed tensor in flat format (batch, sequence x features).
+    """
+
+    if not is_sequence(x):
+        raise ValueError(
+            'attempt to reshape tensor from sequence format to flat format failed. ',
+            f'Excepted input tensor with 3 dimensions, got {x.ndim}.'
+        )
+
+    return x.flatten(start_dim=1)
+
+
+def flat2seq(x: Tensor, num_features: int) -> Tensor:
+    """Reshapes tensor from flat format to sequence format.
+
+    Flat format: (batch, sequence x features)
+    Sequence format: (batch, sequence, features)
+
+    Args:
+        x (Tensor): a tensor in the flat format (batch, sequence x features).
+        num_features (int): number of features (last dimension) of the output tensor.
+
+    Returns:
+        Tensor: the transformed tensor in sequence format (batch, seq, features).
+    """
+
+    if not is_flat(x):
+        raise ValueError(
+            'attempt to reshape tensor from flat format to sequence format failed. ',
+            f'Excepted input tensor with 2 dimensions, got {x.ndim}.'
+        )
+
+    return x.view(x.shape[0], -1, num_features)
