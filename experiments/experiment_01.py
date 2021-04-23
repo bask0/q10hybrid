1cc5c7369abf2491e59792c8ea0b72f1c1a49e3f
Basil Kraft <bkraft@bgc-jena.mpg.de>
2021-04-23 21:24:48 +0200

    updates experiment 1

--- experiments/experiment_01.py
+++ experiments/experiment_01.py
@@ -9,16 +9,15 @@ if os.path.isdir(log_dir):
 fix_args = [
     # '--fast_dev_run 1'
     '--limit_train_batches 0.1',
-    '--max_epochs 1',
+    '--max_epochs 25',
     '--log_every_n_steps 1',
     f'--default_root_dir {log_dir}'
 ]
 
 cmd = 'python train.py ' + ' '.join(fix_args)
 
-#for qinit in [0.5, 1.0, 1.5, 2.0]:
-for qinit in [0.5]:
-    for i in range(1):
+for qinit in [0.5, 1.0, 1.5, 2.0]:
+    for i in range(10):
         dynamic_args = [
             f'--q10_init {qinit}'
         ]
