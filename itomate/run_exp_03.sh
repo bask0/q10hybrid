326bf118c3c13716194f29c384db51ca94aad3c1
Basil Kraft <bkraft@bgc-jena.mpg.de>
2021-04-27 12:21:00 +0200

    delete experiment 3

--- itomate/run_exp_03.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source /Net/Groups/BGI/people/bkraft/anaconda3/etc/profile.d/conda.sh
-conda activate q10hybrid
-cd /Net/Groups/BGI/people/bkraft/git/q10hybrid/
-
-CUDA_VISIBLE_DEVICES="$1" python experiments/experiment_03.py "${@:2}"
