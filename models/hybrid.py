84aef4e8a2ef648ea08cea763198ab62a72c0938
Basil Kraft <bkraft@bgc-jena.mpg.de>
2021-05-05 16:42:12 +0200

    bug: adds learning_rate and weight_decay as arguments

--- models/hybrid.py
+++ models/hybrid.py
@@ -24,6 +24,8 @@ class Q10Model(pl.LightningModule):
             q10_init: int = 1.5,
             hidden_dim: int = 128,
             num_layers: int = 2,
+            learning_rate: float = 0.01,
+            weight_decay: float = 0.,
             dropout: float = 0.,
             activation: bool = 'relu',
             num_steps: int = 0) -> None:
